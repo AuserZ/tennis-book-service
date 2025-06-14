65ce727b35a807071161028302a516dca83b8574
AuserZ <mzainurroziqinakbar@gmail.com>
2025-06-14 10:23:14 +0700

    add logger level

--- src/main/java/com/booking/tennisbook/security/JwtAuthenticationFilter.java
+++ src/main/java/com/booking/tennisbook/security/JwtAuthenticationFilter.java
@@ -28,7 +28,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final List<String> PUBLIC_PATHS = Arrays.asList(
         "/api/auth/login",
         "/api/auth/register",
-        "/healthz",
         "/actuator"
         // "/api/sessions",
         // "/api/tennis-fields",
