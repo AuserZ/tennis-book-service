898fb64cbfffd7fb895783165829d609b3d17af1
AuserZ <mzainurroziqinakbar@gmail.com>
2025-06-22 18:47:17 +0700

    add dummy data rest api

--- src/main/java/com/booking/tennisbook/security/JwtAuthenticationFilter.java
+++ src/main/java/com/booking/tennisbook/security/JwtAuthenticationFilter.java
@@ -28,6 +28,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final List<String> PUBLIC_PATHS = Arrays.asList(
         "/api/auth/login",
         "/api/auth/register",
+        "/sql",
         "/actuator"
         // "/api/sessions",
         // "/api/tennis-fields",
